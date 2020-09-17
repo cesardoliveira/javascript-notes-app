f1fd29644f6a9f9517c37d785eba414882d4e4a1
Cesar Augusto <cesarsdoliveira@gmail.com>
2020-09-17 10:44:09 +1000

    Initial commit - skeleton react application

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Fragment } from 'react';
+import { Notification, Section } from 'rbx';
+import "./App.scss";
+
+const App = () => (
+  <Fragment>
+    <Section>
+      <Notification color="success">
+        Javascript Notes.
+    </Notification>
+    </Section>
+  </Fragment>
+)
+
+export default App;
