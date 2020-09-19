2f1aa644a74243b09faec1d451ef6a108c8f0dc3
Cesar Augusto <cesarsdoliveira@gmail.com>
2020-09-19 16:18:37 +1000

    Header logIn  e logOut

--- src/screens/notes/index/index.js
+++ src/screens/notes/index/index.js
@@ -1,7 +1,9 @@
 import React, { Fragment } from 'react';
+import HeaderLogged from '../../../components/header_logged';
 
 const NotesScreen = () => (
     <Fragment>
+        <HeaderLogged/>
         Notes Screen
     </Fragment>
 );
