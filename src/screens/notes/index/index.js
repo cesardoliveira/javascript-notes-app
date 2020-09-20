6783532d9dc4c61d0b7c5032bc740b3457ffda28
Cesar Augusto <cesarsdoliveira@gmail.com>
2020-09-20 11:02:29 +1000

    Creating Note Screen

--- src/screens/notes/index/index.js
+++ src/screens/notes/index/index.js
@@ -1,11 +1,18 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
+import '../../../styles/notes.scss'
 import HeaderLogged from '../../../components/header_logged';
+import Notes from '../../../components/notes';
 
-const NotesScreen = () => (
-    <Fragment>
-        <HeaderLogged/>
+const NotesScreen = () => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    return (
+        <Fragment>
+            <HeaderLogged setIsOpen={setIsOpen} />
+            <Notes setIsOpen={setIsOpen} isOpen={isOpen} />
         Notes Screen
-    </Fragment>
-);
+        </Fragment>
+    );
+}
 
 export default NotesScreen;
